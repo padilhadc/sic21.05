b2a800ec19b9ed3a977a3bea85ac0520883ff779
padilhadc <114955291+padilhadc@users.noreply.github.com>
2025-05-28 14:17:53 -0300

    Updated ServiceForm.tsx

--- src/pages/ServiceForm.tsx
+++ src/pages/ServiceForm.tsx
@@ -674,7 +674,7 @@ export default function ServiceForm() {
                     required
                     className="w-24 rounded-lg border-gray-300 dark:border-gray-600 shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:text-white sm:text-sm"
                   >
-                    <option value="1x16">  </option>
+                    <option value="1x16"> - </option>
                     <option value="1x8">1x8</option>
                     <option value="1x16">1x16</option>
                     
